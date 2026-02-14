e004d784792ab8d53937d3f3f7ee0979ee174d93
Daniel Meszaros <easimer@gmail.com>
2026-02-14 22:27:14 +0100

    std: add pthread impl of barriers

--- src/std/os/Sync.c
+++ src/std/os/Sync.c
@@ -11,6 +11,37 @@
 #include <std/os/Sync.h>
 
 #if SN_STD_SYSTEM_HAS_PTHREADS
+#include <pthread.h>
+
+struct Barrier {
+  pthread_barrier_t inner;
+};
+
+Barrier *barrierCreate(Arena *arena, u32 count) {
+  Barrier *b = (Barrier *)alloc(arena, sizeof(Barrier), __alignof(Barrier), 1);
+
+  int rc = pthread_barrier_init(&b->inner, NULL, (unsigned int)count);
+  DCHECK(rc == 0);
+  if (rc != 0) {
+    return NULL;
+  }
+
+  return b;
+}
+
+void barrierSync(Barrier *barrier) {
+  int rc = pthread_barrier_wait(&barrier->inner);
+  if (rc == PTHREAD_BARRIER_SERIAL_THREAD || rc == 0) {
+    return;
+  }
+  DCHECK(rc == 0);
+}
+
+void barrierDestroy(Barrier *barrier) {
+  int rc = pthread_barrier_destroy(&barrier->inner);
+  DCHECK(rc == 0);
+}
+
 #elif SN_STD_SYSTEM_WINDOWS
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
