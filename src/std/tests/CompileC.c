d1bfc190b6fda757b05d2de73c9f23702e6a0dbb
Daniel Meszaros <easimer@gmail.com>
2026-03-10 23:18:22 +0100

    std: test that .h files do compile with a C compiler

---
+++ src/std/tests/CompileC.c
@@ -0,0 +1,13 @@
+// Header files that are supposed to compile under C
+
+#include <std/Arena.h>
+#include <std/Check.h>
+#include <std/Chronometry.h>
+#include <std/Hash.h>
+#include <std/log.h>
+#include <std/Modules.h>
+#include <std/Sanitizer.h>
+#include <std/Types.h>
+
+#include <std/os/OsInfo.h>
+#include <std/os/Sync.h>
