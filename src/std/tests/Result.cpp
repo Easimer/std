ad5269cd19ba2ec4fae227db52be975c9a05c670
Daniel Meszaros <easimer@gmail.com>
2026-06-02 21:02:39 +0200

    std: rename test suite Error to Result

--- src/std/tests/Result.cpp
+++ src/std/tests/Result.cpp
@@ -27,23 +27,23 @@ static const i32 RC = -6;
 
 // INIT WITH VALUE
 
-SN_TEST(Error, ctor) {
+SN_TEST(Result, ctor) {
   auto res = Result<Value, TestError>(Value(X));
   CHECK_HAS_VALUE(res, X);
 }
 
-SN_TEST(Error, moveCtor) {
+SN_TEST(Result, moveCtor) {
   Value v(X);
   auto res = Result<Value, TestError>(std::move(v));
   CHECK_HAS_VALUE(res, X);
 }
 
-SN_TEST(Error, assign) {
+SN_TEST(Result, assign) {
   Result<Value, TestError> res = Value(X);
   CHECK_HAS_VALUE(res, X);
 }
 
-SN_TEST(Error, moveAssign) {
+SN_TEST(Result, moveAssign) {
   Value v(X);
   Result<Value, TestError> res = std::move(v);
   CHECK_HAS_VALUE(res, X);
@@ -51,29 +51,29 @@ SN_TEST(Error, moveAssign) {
 
 // INIT WITH ERROR
 
-SN_TEST(Error, ctorErr) {
+SN_TEST(Result, ctorErr) {
   auto res = Result<Value, TestError>(TestError{RC});
   CHECK_HAS_ERROR(res, RC);
 }
 
-SN_TEST(Error, moveCtorErr) {
+SN_TEST(Result, moveCtorErr) {
   TestError v(RC);
   auto res = Result<Value, TestError>(std::move(v));
   CHECK_HAS_ERROR(res, RC);
 }
 
-SN_TEST(Error, assignErr) {
+SN_TEST(Result, assignErr) {
   Result<Value, TestError> res = TestError(RC);
   CHECK_HAS_ERROR(res, RC);
 }
 
-SN_TEST(Error, moveAssignErr) {
+SN_TEST(Result, moveAssignErr) {
   TestError v(RC);
   Result<Value, TestError> res = std::move(v);
   CHECK_HAS_ERROR(res, RC);
 }
 
-SN_TEST(Error, flattenInnerValue) {
+SN_TEST(Result, flattenInnerValue) {
   Result<Value, TestError> inner = Value(X);
 
   Result<Result<Value, TestError>, TestError> outer = inner;
@@ -82,7 +82,7 @@ SN_TEST(Error, flattenInnerValue) {
   CHECK_HAS_VALUE(flattened, X);
 }
 
-SN_TEST(Error, flattenInnerError) {
+SN_TEST(Result, flattenInnerError) {
   Result<Value, TestError> inner = TestError(RC);
 
   Result<Result<Value, TestError>, TestError> outer = inner;
@@ -91,7 +91,7 @@ SN_TEST(Error, flattenInnerError) {
   CHECK_HAS_ERROR(flattened, RC);
 }
 
-SN_TEST(Error, flattenOuterError) {
+SN_TEST(Result, flattenOuterError) {
   Result<Result<Value, TestError>, TestError> outer = TestError(RC);
 
   Result<Value, TestError> flattened = flatten(outer);
