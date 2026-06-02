6c2fb882f78bc08d158d04c3a259f81007b749bb
Daniel Meszaros <easimer@gmail.com>
2026-06-02 22:04:50 +0200

    ci: pretty test output for CI

--- src/std/Testing.hpp
+++ src/std/Testing.hpp
@@ -16,21 +16,24 @@ using SnTestFunc = void (*)(void);
 extern SnTest *gSnTestFirst;
 extern SnTest *gSnTestPrev;
 
-struct SnTest {
-  SnTest *next = nullptr;
+struct SnTestMetadata {
   const char *suiteName;
   const char *name;
+
+  const char *file;
+  int line;
+};
+
+struct SnTest {
+  SnTest *next;
+  const SnTestMetadata *metadata;
   bool shouldPass;
 
   void (*pfnTest)(void);
 
-  SnTest(const char *suiteName,
-         const char *name,
-         void (*pfnTest)(void),
-         bool shouldPass)
+  SnTest(const SnTestMetadata *metadata, void (*pfnTest)(void), bool shouldPass)
       : next(nullptr),
-        suiteName(suiteName),
-        name(name),
+        metadata(metadata),
         shouldPass(shouldPass),
         pfnTest(pfnTest) {
     if (gSnTestFirst != nullptr) {
@@ -63,9 +66,15 @@ struct SnTestResult {
 
 #if SN_TEST_EXECUTABLE
 /** \brief Creates a test definition */
-#define SN_TEST_DEFINE_DESC(SuiteName, TestName, ShouldPass)            \
-  static SnTest test_##SuiteName##_##TestName =                         \
-      SnTest(SN_TEST_STRINGIFY(SuiteName), SN_TEST_STRINGIFY(TestName), \
+#define SN_TEST_DEFINE_DESC(SuiteName, TestName, ShouldPass)         \
+  static const SnTestMetadata test_meta_##SuiteName##_##TestName = { \
+      .suiteName = SN_TEST_STRINGIFY(SuiteName),                     \
+      .name = SN_TEST_STRINGIFY(TestName),                           \
+      .file = __FILE__,                                              \
+      .line = __LINE__,                                              \
+  };                                                                 \
+  static SnTest test_##SuiteName##_##TestName =                      \
+      SnTest(&test_meta_##SuiteName##_##TestName,                    \
              test_func_##SuiteName##_##TestName, ShouldPass)
 #else
 #define SN_TEST_DEFINE_DESC(SuiteName, TestName, ShouldPass)
@@ -83,76 +92,3 @@ struct SnTestResult {
   SN_TEST_DEFINE_DESC(SuiteName, TestName, false); \
   static void test_func_##SuiteName##_##TestName(void)
 
-#define SN_TEST_MAIN                                                           \
-  SnTest *gSnTestFirst = nullptr;                                              \
-  SnTest *gSnTestPrev = nullptr;                                               \
-  static std::jmp_buf gJmpBuf;                                                 \
-  extern "C" void checkFail(const char *pExpr, const char *pFile,              \
-                            unsigned line) {                                   \
-    log_fatal("\n  Assertion failed: %s\n    at %s:%u\n", pExpr, pFile, line); \
-    std::longjmp(gJmpBuf, 1);                                                  \
-  }                                                                            \
-                                                                               \
-  extern "C" void handleOOM(Arena *arena) {                                    \
-    log_fatal("\n  Arena %p is out of memory", arena);                         \
-    std::longjmp(gJmpBuf, 1);                                                  \
-  }                                                                            \
-  extern "C" SnTestResult *testMain(Arena *arena0, Arena *arena1,              \
-                                    Arena *arenaResults, SnTestStats *stats) { \
-    SnTestResult *res = nullptr;                                               \
-    SnTestResult *ret = nullptr;                                               \
-    setAllocatorsForThread(arena0, arena1);                                    \
-                                                                               \
-    SnTest *currentTest = gSnTestFirst;                                        \
-    bool didPass = false;                                                      \
-                                                                               \
-    u32 numSuccess = 0;                                                        \
-    u32 numTotal = 0;                                                          \
-    TimePoint t_start;                                                         \
-    TimePoint t_end;                                                           \
-    SnTestResult *prev;                                                        \
-                                                                               \
-    while (currentTest != nullptr) {                                           \
-      prev = res;                                                              \
-      res = alloc<SnTestResult>(arenaResults);                                 \
-      if (prev != nullptr) {                                                   \
-        prev->next = res;                                                      \
-      } else {                                                                 \
-        ret = res;                                                             \
-      }                                                                        \
-      res->next = nullptr;                                                     \
-      res->test = currentTest;                                                 \
-      res->ok = false;                                                         \
-      res->duration = 0;                                                       \
-                                                                               \
-      didPass = false;                                                         \
-                                                                               \
-      if (!setjmp(gJmpBuf)) {                                                  \
-        t_start = chrono_getCurrentTime();                                     \
-        currentTest->pfnTest();                                                \
-        /* Check leaks */                                                      \
-        CHECK((arena0->end - arena0->beg) == SIZ_ARENA);                       \
-        CHECK((arena1->end - arena1->beg) == SIZ_ARENA);                       \
-        didPass = true;                                                        \
-      }                                                                        \
-      t_end = chrono_getCurrentTime();                                         \
-      if (currentTest->shouldPass == didPass) {                                \
-        res->ok = true;                                                        \
-        numSuccess += 1;                                                       \
-      } else {                                                                 \
-        printf("=== FAILED: [%s] %s ===\n", currentTest->suiteName,            \
-               currentTest->name);                                             \
-      }                                                                        \
-      res->duration = chrono_secondsBetween(t_start, t_end);                   \
-      numTotal += 1;                                                           \
-      currentTest = currentTest->next;                                         \
-    }                                                                          \
-    stats->numSuccess = numSuccess;                                            \
-    stats->numTotal = numTotal;                                                \
-    return ret;                                                                \
-  }
-
-extern "C" SnTestResult *testMain(Arena *arena0,
-                                  Arena *arena1,
-                                  Arena *arenaResults,
-                                  SnTestStats *stats);
--- src/test_exe/entry.cpp
+++ src/test_exe/entry.cpp
@@ -11,10 +11,49 @@
 #include <std/Chronometry.h>
 #include <std/log.h>
 #include <std/Slice.hpp>
+#include <std/SliceUtils.hpp>
 #include <std/Testing.hpp>
 
 #include <stdio.h>
 #include <stdlib.h>
+
+SnTest *gSnTestFirst = nullptr;
+SnTest *gSnTestPrev = nullptr;
+static std::jmp_buf gJmpBuf;
+static bool gSnRunningInGA = false;
+static Slice<char> gSnCwd;
+
+Slice<char> getWorkingDir(Slice<char> dst, u32 &sizRequired);
+
+#if WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+Slice<char> getWorkingDir(Slice<char> dst, u32 &sizRequired) {
+  DWORD rc = GetCurrentDirectoryA(dst.length, dst.data);
+  if (rc == 0) {
+    sizRequired = 0;
+    return {};
+  }
+
+  sizRequired = rc;
+  return dst.subarray(0, rc);
+}
+
+#else
+#include <unistd.h>
+Slice<char> getWorkingDir(Slice<char> dst, u32 &sizRequired) {
+  char *res = getcwd(dst.data, dst.length);
+  if (res == nullptr) {
+    sizRequired = dst.length != 0 ? dst.length * 2 : 1;
+    return {};
+  }
+
+  u32 len = strlen(res);
+  sizRequired = len;
+  return dst.subarray(0, len);
+}
+#endif
 
 static const u32 SIZ_ARENA = 256 * 1024;
 
@@ -24,7 +63,98 @@ static Arena makeArena() {
   return {base, end};
 }
 
-SN_TEST_MAIN;
+static Slice<const char> getRepoRelativePath(Slice<const char> file) {
+  if (!file.startsWith(gSnCwd.asConst())) {
+    return file;
+  }
+
+  return file.subarray(gSnCwd.length + 1);
+}
+
+extern "C" void checkFail(const char *pExpr, const char *pFile, unsigned line) {
+  if (gSnRunningInGA) {
+    Slice<const char> pathFileRelRepo =
+        getRepoRelativePath({pFile, (u32)strlen(pFile)});
+    printf("::error file=%.*s,line=%u::Assertion failed: %s\n",
+           FMT_SLICE(pathFileRelRepo), line, pExpr);
+  } else {
+    log_fatal("\n  Assertion failed: %s\n    at %s:%u\n", pExpr, pFile, line);
+  }
+  std::longjmp(gJmpBuf, 1);
+}
+
+extern "C" void handleOOM(Arena *arena) {
+  log_fatal("\n  Arena %p is out of memory", arena);
+  std::longjmp(gJmpBuf, 1);
+}
+
+extern "C" SnTestResult *testMain(Arena *arena0,
+                                  Arena *arena1,
+                                  Arena *arenaResults,
+                                  SnTestStats *stats) {
+  SnTestResult *res = nullptr;
+  SnTestResult *ret = nullptr;
+  setAllocatorsForThread(arena0, arena1);
+
+  SnTest *currentTest = gSnTestFirst;
+  bool didPass = false;
+
+  u32 numSuccess = 0;
+  u32 numTotal = 0;
+  TimePoint t_start = chrono_getCurrentTime();
+  TimePoint t_end;
+  SnTestResult *prev;
+
+  while (currentTest != nullptr) {
+    prev = res;
+    res = alloc<SnTestResult>(arenaResults);
+    if (prev != nullptr) {
+      prev->next = res;
+    } else {
+      ret = res;
+    }
+    res->next = nullptr;
+    res->test = currentTest;
+    res->ok = false;
+    res->duration = 0;
+
+    didPass = false;
+
+    const SnTestMetadata *meta = currentTest->metadata;
+    if (gSnRunningInGA) {
+      printf("::group::[%s] %s\n", meta->suiteName, meta->name);
+    }
+
+    if (!setjmp(gJmpBuf)) {
+      t_start = chrono_getCurrentTime();
+      currentTest->pfnTest();
+      // Check for leaks
+      CHECK((arena0->end - arena0->beg) == SIZ_ARENA);
+      CHECK((arena1->end - arena1->beg) == SIZ_ARENA);
+      didPass = true;
+    }
+    t_end = chrono_getCurrentTime();
+    if (currentTest->shouldPass == didPass) {
+      res->ok = true;
+      numSuccess += 1;
+    } else {
+      if (!gSnRunningInGA) {
+        printf("=== FAILED: [%s] %s ===\n", meta->suiteName, meta->name);
+        printf("  Defined at %s:%d\n", meta->file, meta->line);
+        printf("===================\n");
+      }
+    }
+    res->duration = chrono_secondsBetween(t_start, t_end);
+    numTotal += 1;
+    currentTest = currentTest->next;
+    if (gSnRunningInGA) {
+      printf("::endgroup\n");
+    }
+  }
+  stats->numSuccess = numSuccess;
+  stats->numTotal = numTotal;
+  return ret;
+}
 
 static void printRes(const SnTestResult *results) {
   if (results->ok) {
@@ -33,9 +163,92 @@ static void printRes(const SnTestResult *results) {
     printf("{ FAIL } ");
   }
 
-  const SnTest *test = results->test;
-  printf("[%s] %s [%.03f ms]\n", test->suiteName, test->name,
+  const SnTestMetadata *meta = results->test->metadata;
+  printf("[%s] %s [%.03f ms]\n", meta->suiteName, meta->name,
          results->duration * 1000.0);
+}
+
+static void printfMarkdownRow(FILE *f,
+                              const SnTestResult *cur,
+                              const char *baseUrl) {
+  const SnTestMetadata *meta = cur->test->metadata;
+  Slice<const char> pathFileRelRepo =
+      getRepoRelativePath({meta->file, (u32)strlen(meta->file)});
+
+  char url[1024];
+  snprintf(url, sizeof(url) - 1, "%s%.*s#L%d", baseUrl,
+           FMT_SLICE(pathFileRelRepo), meta->line);
+  url[1023] = '\0';
+
+  f64 duration_ms = cur->duration * 1000.0;
+
+  fprintf(f, "| `%s` | `%s` | %f ms | [%.*s#L%d](%s) |\n", meta->suiteName,
+          meta->name, duration_ms, FMT_SLICE(pathFileRelRepo), meta->line, url);
+}
+
+static void generateActionsJobSummary(const SnTestStats *stats,
+                                      SnTestResult *results) {
+  const char *pathSummary = getenv("GITHUB_STEP_SUMMARY");
+  const char *serverUrl = getenv("GITHUB_SERVER_URL");
+  const char *repository = getenv("GITHUB_REPOSITORY");
+  const char *commit = getenv("GITHUB_SHA");
+  if (pathSummary == nullptr || serverUrl == nullptr || repository == nullptr ||
+      commit == nullptr) {
+    return;
+  }
+
+  char baseUrl[256];
+  snprintf(baseUrl, 255, "%s/%s/tree/%s/", serverUrl, repository, commit);
+
+  FILE *f = fopen(pathSummary, "wb");
+  if (f == nullptr) {
+    return;
+  }
+
+  u32 numFailed = stats->numTotal - stats->numSuccess;
+
+  fprintf(f, "# %s\n", repository);
+  fprintf(f, "|  |  |\n");
+  fprintf(f, "| --- | --- |\n");
+  fprintf(f, "| **Successful tests** | %u |\n", stats->numSuccess);
+  fprintf(f, "| **Failed tests** | %u |\n", numFailed);
+  fprintf(f, "| **Total** | %u |\n", stats->numTotal);
+
+  if (numFailed != 0) {
+    fprintf(f, "## Failed tests\n");
+    fprintf(f, "| Suite | Name | Duration | Definition |\n");
+    fprintf(f, "| --- | --- | --- | --- |\n");
+    SnTestResult *cur = results;
+    while (cur != nullptr) {
+      if (cur->ok) {
+        cur = cur->next;
+        continue;
+      }
+
+      printfMarkdownRow(f, cur, baseUrl);
+
+      cur = cur->next;
+    }
+  }
+
+  {
+    fprintf(f, "## Successful tests\n");
+    fprintf(f, "| Suite | Name | Duration | Definition |\n");
+    fprintf(f, "| --- | --- | --- | --- |\n");
+    SnTestResult *cur = results;
+    while (cur != nullptr) {
+      if (!cur->ok) {
+        cur = cur->next;
+        continue;
+      }
+
+      printfMarkdownRow(f, cur, baseUrl);
+
+      cur = cur->next;
+    }
+  }
+
+  fclose(f);
 }
 
 int main(int numArgs, char **arrArgs) {
@@ -43,16 +256,42 @@ int main(int numArgs, char **arrArgs) {
   Arena arena1 = makeArena();
   Arena arenaResults = makeArena();
 
+  Slice<char> cwd;
+  u32 sizRequired = 1;
+  while (cwd.length < sizRequired) {
+    Arena saved = arenaResults;
+    alloc(&arenaResults, sizRequired, cwd);
+    cwd = getWorkingDir(cwd, sizRequired);
+
+    if (cwd.length < sizRequired) {
+      arenaResults = saved;
+    }
+  };
+
+  gSnCwd = cwd;
+  printf("Working directory: %.*s\n", FMT_SLICE(cwd));
+
+  const char *ga = getenv("CI");
+  if (ga != nullptr) {
+    gSnRunningInGA = true;
+  }
+
+  printf("Running in GA: %d\n", gSnRunningInGA ? 1 : 0);
+
   SnTestStats stats;
   TimePoint t_start = chrono_getCurrentTime();
-  SnTestResult* results = testMain(&arena0, &arena1, &arenaResults, &stats);
+  SnTestResult *const results =
+      testMain(&arena0, &arena1, &arenaResults, &stats);
   TimePoint t_end = chrono_getCurrentTime();
 
   u32 numFail = stats.numTotal - stats.numSuccess;
-  f32 percentSuccess = stats.numSuccess / f32(stats.numTotal) * 100.0f;
-  f32 percentFail = numFail / f32(stats.numTotal) * 100.0f;
+  f64 percentSuccess = stats.numSuccess / f64(stats.numTotal) * 100.0;
+  f64 percentFail = numFail / f64(stats.numTotal) * 100.0;
 
   printf("\n");
+  if (gSnRunningInGA) {
+    printf("::group::Summary\n");
+  }
 
   if (numFail != 0) {
     // Print failed tests first
@@ -88,12 +327,20 @@ int main(int numArgs, char **arrArgs) {
   printf("Failed tests:     %u/%u (%.2f%%)\n", numFail, stats.numTotal,
          percentFail);
 
+  if (gSnRunningInGA) {
+    generateActionsJobSummary(&stats, results);
+  }
+
   free(arenaResults.beg);
   free(arena1.beg);
   free(arena0.beg);
 
   f64 elapsed = chrono_secondsBetween(t_start, t_end);
   printf("Duration:         %f milliseconds\n", elapsed * 1000.0);
+
+  if (gSnRunningInGA) {
+    printf("::endgroup\n");
+  }
 
   int rc = (numFail == 0) ? 0 : 1;
   return rc;
