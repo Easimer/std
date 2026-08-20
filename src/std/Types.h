ca36b3ab6896dee99b555bc1cac783eb363231d9
Daniel Meszaros <easimer@gmail.com>
2026-08-20 19:22:10 +0200

    std: make countLeadingZeros static-inline

--- src/std/Types.h
+++ src/std/Types.h
@@ -11,6 +11,10 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#if defined(_MSC_VER)
+#include <intrin.h>
+#endif
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -32,13 +36,48 @@ extern "C" {
  * \returns The number of leading zeros. When x is zero, the result is 32,
  * similarly to Rust's `u32::leading_zeros`.
  */
-i32 countLeadingZeros(u32 x);
+static inline i32 countLeadingZeros(u32 x) {
+#if defined(_MSC_VER)
+  unsigned long index;
+  if (_BitScanReverse(&index, x)) {
+    // `index` contains the position of the first set bit, which is counted from
+    // the LSB. Compute the number of leading zeros:
+    return 32 - index - 1;
+  } else {
+    return 32;
+  }
+#else
+  if (x == 0) {
+    return 32;
+  }
+
+  return (i32)__builtin_clz(x);
+#endif
+}
+
 /**
  * \brief Count leading zeros.
  * \returns The number of leading zeros. When x is zero, the result is 64,
  * similarly to Rust's `u64::leading_zeros`.
  */
-i32 countLeadingZeros64(u64 x);
+static inline i32 countLeadingZeros64(u64 x) {
+#if defined(_MSC_VER)
+  unsigned long index;
+  if (_BitScanReverse64(&index, x)) {
+    // `index` contains the position of the first set bit, which is counted from
+    // the LSB. Compute the number of leading zeros:
+    return 64 - index - 1;
+  } else {
+    return 64;
+  }
+#else
+  if (x == 0) {
+    return 64;
+  }
+
+  return (i32)__builtin_clzll(x);
+#endif
+}
 
 u8 mipmapGetLevelCount(u32 width, u32 height);
 u8 mipmapGetLevelCount3(u32 width, u32 height, u32 depth);
